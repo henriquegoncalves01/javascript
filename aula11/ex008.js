0cfffe09fb5258d1004e3d0b51190f487c011aab
Henrique Gonçalves Mantovanelli <cellrick43@gmail.com>
2026-04-22 19:33:08 -0300

    Condiçao IF em javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade de seu carro e ${vel}km/h`)
+if (vel > 60){
+    console.log('voce utrapassol a velocidade permitida voce foi multado!')
+}
+console.log('dirija usando sempre um sinto de segurança')
